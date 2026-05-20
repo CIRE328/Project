fe01301a6b94f019dfae3bbc5a996e675371607f
CIRE328 <1591907523@qq.com>
2026-05-20 20:56:58 +0800

    Project Version 1.0

--- src/pojo/Bed.java
+++ src/pojo/Bed.java
@@ -1,62 +1,47 @@
 package pojo;
 
 public class Bed {
-    private int id;
-    private int roomNo;
-    private int bedStatus;
+    private Integer id;
+    private Integer roomNo;
+    private Integer bedStatus;
     private String remarks;
     private String bedNo;
+    private Boolean isDeleted;   // 补充逻辑删除字段
 
-    public Bed(){}
+    public Bed() {}
 
-    public Bed(int id, int roomNo, int bedStatus, String remarks, String bedNo) {
+    public Bed(Integer id, Integer roomNo, Integer bedStatus, String remarks, String bedNo, Boolean isDeleted) {
         this.id = id;
         this.roomNo = roomNo;
         this.bedStatus = bedStatus;
         this.remarks = remarks;
         this.bedNo = bedNo;
+        this.isDeleted = isDeleted;
     }
 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getRoomNo() {
-        return roomNo;
-    }
-    public void setRoomNo(int roomNo) {
-        this.roomNo = roomNo;
-    }
-    public int getBedStatus() {
-        return bedStatus;
-    }
-    public void setBedStatus(int bedStatus) {
-        this.bedStatus = bedStatus;
-    }
-    public String getRemarks() {
-        return remarks;
-    }
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
-    }
-    public String getBedNo() {
-        return bedNo;
-    }
-    public void setBedNo(String bedNo) {
-        this.bedNo = bedNo;
-    }
+    // getter/setter 使用包装类
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public Integer getRoomNo() { return roomNo; }
+    public void setRoomNo(Integer roomNo) { this.roomNo = roomNo; }
+    public Integer getBedStatus() { return bedStatus; }
+    public void setBedStatus(Integer bedStatus) { this.bedStatus = bedStatus; }
+    public String getRemarks() { return remarks; }
+    public void setRemarks(String remarks) { this.remarks = remarks; }
+    public String getBedNo() { return bedNo; }
+    public void setBedNo(String bedNo) { this.bedNo = bedNo; }
+    public Boolean getIsDeleted() { return isDeleted; }
+    public void setIsDeleted(Boolean isDeleted) { this.isDeleted = isDeleted; }
 
     @Override
     public String toString() {
         return "Bed{" +
                 "id=" + id +
-                ", roomNo='" + roomNo + '\'' +
-                ", bedStatus='" + bedStatus + '\'' +
+                ", roomNo=" + roomNo +
+                ", bedStatus=" + bedStatus +
                 ", remarks='" + remarks + '\'' +
-                ", bedNo=" + bedNo +
+                ", bedNo='" + bedNo + '\'' +
+                ", isDeleted=" + isDeleted +
                 '}';
     }
-
 }
